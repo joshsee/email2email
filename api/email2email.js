5c0a05adc57834c2d13be3cf42f875bd986f0d94
joshsee <ychian@gmail.com>
2020-12-24 19:50:36 +0800

    Add from email in subject

--- api/email2email.js
+++ api/email2email.js
@@ -22,7 +22,7 @@ module.exports = async (req, res) => {
     const email = {
         to: process.env.TO_EMAIL_ADDRESS,
         from: process.env.FROM_EMAIL_ADDRESS,
-        subject: `${subject}`,
+        subject: `${subject} ${fromAddress}`,
         text: `${body}`,
         html: `${html}`,
     };
