32d44f11587a1cf9f0a7d3afd7c55cafb1c6f3bf
joshsee <ychian@gmail.com>
2022-05-29 16:07:45 +0800

    Added rest and bar domain

--- api/email2email.js
+++ api/email2email.js
@@ -27,7 +27,7 @@ module.exports = async (req, res) => {
         html: `${html}`,
     };
 
-    var patt = new RegExp("\.(buzz|guru|cyou|biz|live|co|us|today|icu)$");
+    var patt = new RegExp("\.(buzz|guru|cyou|biz|live|co|us|today|icu|rest|bar)$");
     if (patt.test(fromAddress.domain)==false) {
         //Send Email
         sgResp = sgMail.send(email)
