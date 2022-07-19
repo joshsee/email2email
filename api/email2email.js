72c12005406d1cb576c60350fa42a073cc52814c
joshsee <ychian@gmail.com>
2022-07-19 21:36:49 +0800

    add click domain

--- api/email2email.js
+++ api/email2email.js
@@ -27,7 +27,7 @@ module.exports = async (req, res) => {
         html: `${html}`,
     };
 
-    var patt = new RegExp("\.(buzz|guru|cyou|biz|live|co|us|today|icu|rest|bar|za.com|ru.com|sa.com)$");
+    var patt = new RegExp("\.(buzz|guru|cyou|biz|live|co|us|today|icu|rest|bar|za.com|ru.com|sa.com|click)$");
     if (patt.test(fromAddress.domain)==false) {
         //Send Email
         sgResp = sgMail.send(email)
