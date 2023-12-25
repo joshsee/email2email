e0e73c3e9807cc1ced57dd1b956668506e02eac3
Josh See <ychian@gmail.com>
2023-12-25 22:36:58 +0800

    Revert

--- api/email2email.js
+++ api/email2email.js
@@ -39,7 +39,7 @@ module.exports = async (req, res) => {
             
             const attachmentNo = `${'attachment' + i}`;
             const attachmentContent = {
-                content: req.files[attachmentNo].toString("base64"),
+                content: req.files[attachmentNo],
                 filename: attachmentInfo[attachmentNo].filename,
                 type: attachmentInfo[attachmentNo].type,
                 disposition: "attachment"
